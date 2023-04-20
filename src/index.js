d3cf53c58f42ddb2663ecddec24061a56423f797
coli <coli@coli.com>
2023-04-20 15:29:59 +0900

    2023-04-20

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import ReactDOM from 'react-dom/client'
 //import App from './1.basic/App'
-import App from './2.tictactoe/App'
+//import App from './2.tictactoe/App'
+//import App from './3.thinking/App'
+import App from './4.hook/App'
+
 
 
 ReactDOM.createRoot(
